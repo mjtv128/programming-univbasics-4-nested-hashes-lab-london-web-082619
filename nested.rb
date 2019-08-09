6b70ec21bd94460672eacd3a80c23879fea5a82d
jocular-source-7570 <mjtvan@gmail.com>
2019-08-09 12:38:59 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,11 +16,11 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+	
 	
 	programmer_hash = 
  		{
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     	programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
@@ -79,6 +83,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",
+  :languages => ["LISP", "C"]}
+  programmer_hash
 
     
 end
@@ -102,6 +110,10 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     programmer_hash
+     
+     
      
 end
 
@@ -122,4 +134,6 @@ def adding_to_dennis
         }
      }
 
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+programmer_hash
 end
